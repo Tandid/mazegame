6bd9474ee09a187ff5fbcc55a0c85aba6f5e1e41
Tandid Alam <tandid.alam@gmail.com>
2021-10-09 00:14:23 -0400

    added win condition

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 10;
 const width = 600;
-const height = 600;
+const height = 800;
 
 const unitLength = width / cells;
 
@@ -127,6 +127,7 @@ horizontals.forEach((row, rowIndex) => {
       unitLength,
       5,
       {
+        label: "wall",
         isStatic: true,
       }
     );
@@ -146,6 +147,7 @@ verticals.forEach((row, rowIndex) => {
       5,
       unitLength,
       {
+        label: "wall",
         isStatic: true,
       }
     );
@@ -161,6 +163,7 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: "goal",
     isStatic: true,
   }
 );
@@ -168,7 +171,9 @@ World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball",
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", (event) => {
@@ -189,4 +194,24 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowLeft") {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y = 1;
+      world.bodies.forEach((body) => {
+        if (body.label === "wall") {
+          Body.setStatic(body, false);
+        }
+      });
+    }
+  });
 });
